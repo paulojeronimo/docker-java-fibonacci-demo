20fb496e712169b6ab5b2850ba069587fe30dc83
Paulo Jerônimo <paulojeronimo@gmail.com>
2023-04-21 05:23:00 -0300

    Changed the name of the method fibonacci to calculate

--- src/main/java/com/paulojeronimo/Fibonacci.java
+++ src/main/java/com/paulojeronimo/Fibonacci.java
@@ -1,12 +1,12 @@
 package com.paulojeronimo;
 
 public class Fibonacci {
-  public static long fibonacci(long n) {
-    return n <= 1 ? n : fibonacci(n-1) + fibonacci(n-2);
+  public static long calculate(long n) {
+    return n <= 1 ? n : calculate(n-1) + calculate(n-2);
   }
 
   public static void main(String[] args) {
     int n = Integer.parseInt(args.length > 0 ? args[0] : "5");
-    System.out.println(String.format("Fibonacci(%s) = %s", n, fibonacci(n)));
+    System.out.println(String.format("Fibonacci(%s) = %s", n, calculate(n)));
   }
 }
